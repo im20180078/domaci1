8dab5358889100fdfd5282fa91d0de3bb7847c61
im20180078 <im20180078@student.fon.bg.ac.rs>
2022-12-03 18:41:07 +0100

    header

---
+++ inc/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+            <div class="container">
+                <a class="navbar-brand" href="#">sajt</a>
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item">
+                    
+                    </li>
+                    <li class="nav-item">
+
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav-item">
+                    
+                    </li>
+                    <li class="nav-item">
+                        <button id="btn-logout" type="button" class="nav-link btn btn-link text-dark">Logout</button>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
